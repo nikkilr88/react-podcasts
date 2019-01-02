dcbff0b962ac8d779716bace4117687976e259c5
Nicole Sattler <nicole.sattler@live.com>
2019-01-02 11:28:47 -0600

    Import fragment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import BoilerplateImage from '../../images/boilerplate-img.svg'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <h1>Parcel React Boilerplate</h1>
         <p>Have fun building! You got this!</p>
         <img src={BoilerplateImage} alt="heart girl" />
-      </div>
+      </Fragment>
     )
   }
 }
