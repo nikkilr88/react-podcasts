7aec5605ebf124f02697ee6dfe66db45ae8744c8
Nicole <nicole.sattler@live.com>
2019-01-05 15:06:21 -0600

    Add moment js to format date

--- src/components/PodcastListElement.js
+++ src/components/PodcastListElement.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 
 class PodcastListElement extends Component {
   render() {
@@ -7,10 +8,12 @@ class PodcastListElement extends Component {
     return (
       <div className="infoBox">
         <div className="text">
-          <p className="date">{date}</p>
-          <h3>{title}</h3>
+          <p className="date">{moment(date).format('LL')}</p>
+          <h3>{title.length > 50 ? title.substring(0, 50) + '...' : title}</h3>
         </div>
-        <button onClick={() => setAudio(audio, title)}>Play</button>
+        <button className="btn" onClick={() => setAudio(audio, title)}>
+          <i className="material-icons">play_arrow</i>
+        </button>
       </div>
     )
   }
