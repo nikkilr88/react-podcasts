29a929d60a8406f149d6af06fb8164adab7d8fed
Nicole <nicole.sattler@live.com>
2019-01-05 15:06:05 -0600

    Add on click functions, change icons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react'
+import Sound from 'react-sound'
 
 class Controls extends Component {
   render() {
-    const { pauseAudio, audio, getAudioTime } = this.props
+    const {
+      playingStatus,
+      pauseAudio,
+      stopAudio,
+      fastforward,
+      rewind,
+      audio,
+      time
+    } = this.props
 
     return (
       <div id="player">
-        <div id="title">{audio.title}</div>
-        <span>{audio.time}</span>
-        <div>
-          <button>
+        <div className="title">
+          {audio.title.length > 50
+            ? audio.title.substring(0, 50) + '...'
+            : audio.title}
+        </div>
+
+        <div className="control-btns">
+          <span className="time">{time}</span>
+          <button onClick={rewind}>
             <i className="material-icons">replay_5</i>
           </button>
 
           <button onClick={pauseAudio}>
-            <i className="material-icons paused">pause</i>
+            {playingStatus == Sound.status.PLAYING ? (
+              <i className="material-icons paused">pause</i>
+            ) : (
+              <i className="material-icons">play_arrow</i>
+            )}
           </button>
 
-          <button>
+          <button onClick={stopAudio}>
+            <i className="material-icons">stop</i>
+          </button>
+
+          <button onClick={fastforward}>
             <i className="material-icons">forward_10</i>
           </button>
         </div>
