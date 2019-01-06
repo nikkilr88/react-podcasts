5ce84859498a9fc9bf46213b8ef47f6b27ebc9f6
Nicole <nicole.sattler@live.com>
2019-01-05 18:01:57 -0600

    Add parallax effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
 import React, { Component, Fragment } from 'react'
 
 class Header extends Component {
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      let starting = 1
+      let scrollPos = document.documentElement.scrollTop
+
+      if (scrollPos > 200) return
+
+      // Move background image up at a slower rate
+      document.querySelector('.header-bg').style.backgroundPosition =
+        '50% ' + (50 + scrollPos / 25 + '%')
+
+      // Fade out text
+      document.querySelector('.channel-title').style.opacity =
+        starting - scrollPos / 100
+    })
+  }
   render() {
     const { title, img } = this.props
 
     const styles = {
       background: `url(${img})`,
       backgroundSize: 'cover',
-      backgroundPosition: 'center'
+      backgroundPosition: '50% 50%'
     }
+
     return (
       <Fragment>
         <header>
