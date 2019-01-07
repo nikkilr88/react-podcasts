ce8027a29be7f3b5c5835ce1a645feb48b53e7e2
Nicole <nicole.sattler@live.com>
2019-01-07 12:22:39 -0600

    Remove state, channel info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,6 @@
 import React, { Component, Fragment } from 'react'
 
 class Header extends Component {
-  state = {
-    showDescription: false
-  }
-
-  toggleDescription = () => {
-    this.setState(() => ({ showDescription: !this.state.showDescription }))
-  }
-
   componentDidMount() {
     document.addEventListener('scroll', () => {
       let starting = 1
@@ -21,21 +13,21 @@ class Header extends Component {
         '50% ' + (50 + scrollPos / 25 + '%')
 
       // Fade out text
-      const descripton = document.querySelector('.description')
-      const title = document.querySelector('.channel-title')
+      // const descripton = document.querySelector('.description')
+      // const title = document.querySelector('.channel-title')
 
-      if (title) {
-        title.style.opacity = starting - scrollPos / 100
-      }
+      // if (title) {
+      //   title.style.opacity = starting - scrollPos / 100
+      // }
 
-      if (descripton) {
-        descripton.style.opacity = starting - scrollPos / 100
-      }
+      // if (descripton) {
+      //   descripton.style.opacity = starting - scrollPos / 100
+      // }
     })
   }
 
   render() {
-    const { title, img, description } = this.props
+    const { img } = this.props
 
     const styles = {
       background: `url(${img})`,
@@ -46,21 +38,9 @@ class Header extends Component {
     return (
       <Fragment>
         <header>
-          <i className="material-icons info" onClick={this.toggleDescription}>
-            info_outline
-          </i>
-          {this.state.showDescription && (
-            <Fragment>
-              <div className="description-wrapper header-bg" style={styles} />
-              <div className="description">
-                {description || <p>No description available</p>}
-              </div>
-            </Fragment>
-          )}
           <div className="bg-wrapper">
             <div style={styles} className="header-bg" />
           </div>
-          <p className="channel-title">{title}</p>
         </header>
         <img className="channel-img" src={img} alt="podcast image" />
       </Fragment>
