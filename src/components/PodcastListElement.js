231e08f7f19c1390973f6f84aa66941dcd137e39
Nicole <nicole.sattler@live.com>
2019-01-06 18:48:30 -0600

    Add handleOnClick, add now playing indicator

--- src/components/PodcastListElement.js
+++ src/components/PodcastListElement.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react'
 import moment from 'moment'
 
 class PodcastListElement extends Component {
+  handleOnClick = e => {
+    this.props.setAudio(this.props.audio, this.props.title)
+
+    const btns = document.querySelectorAll('.btn')
+    for (let btn of btns) {
+      btn.classList.remove('selected')
+      btn.innerHTML = '<i class="material-icons">play_arrow</i>'
+    }
+    e.target.classList.add('selected')
+    e.target.innerHTML = '<i class="material-icons">volume_up</i>'
+  }
   render() {
-    const { title, date, audio, setAudio } = this.props
+    const { title, date } = this.props
 
     return (
       <div className="infoBox">
@@ -11,7 +22,7 @@ class PodcastListElement extends Component {
           <p className="date">{moment(date).format('LL')}</p>
           <h3>{title.length > 50 ? title.substring(0, 50) + '...' : title}</h3>
         </div>
-        <button className="btn" onClick={() => setAudio(audio, title)}>
+        <button className="btn" onClick={this.handleOnClick}>
           <i className="material-icons">play_arrow</i>
         </button>
       </div>
