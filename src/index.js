381d3373f7ab12a56976a3363dfafd8b4d0ccd93
Nicole <nicole.sattler@live.com>
2019-01-07 20:42:00 -0600

    Import CSS

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+import './css/styles.css'
+
 ReactDOM.render(<App />, document.getElementById('root'))
