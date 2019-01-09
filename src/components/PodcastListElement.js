a93cd5a8c26224317ae513cf83ba8f6eb4aa1110
Nicole <nicole.sattler@live.com>
2019-01-09 15:04:02 -0600

    Add now playing indicator logic to handle page changes

--- src/components/PodcastListElement.js
+++ src/components/PodcastListElement.js
@@ -8,8 +8,10 @@ class PodcastListElement extends Component {
     this.props.setAudio(this.props.audio, this.props.title)
     this.props.resetButtons(e)
   }
+
   render() {
     const { title, date } = this.props
+    const isPlaying = this.props.title == this.props.nowPlaying
 
     return (
       <div className="infoBox">
@@ -17,8 +19,15 @@ class PodcastListElement extends Component {
           <p className="date">{moment(date).format('LL')}</p>
           <h3>{title.length > 50 ? title.substring(0, 50) + '...' : title}</h3>
         </div>
-        <button className="btn" onClick={this.handleOnClick}>
-          <i className="material-icons">play_arrow</i>
+        <button
+          className={isPlaying ? 'btn selected' : 'btn'}
+          onClick={this.handleOnClick}
+        >
+          {isPlaying ? (
+            <i className="material-icons">volume_up</i>
+          ) : (
+            <i className="material-icons">play_arrow</i>
+          )}
         </button>
       </div>
     )
