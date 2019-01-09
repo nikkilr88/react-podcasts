0745f4a67647d756ad31fe7cf4c960f57ad11b7c
Nicole <nicole.sattler@live.com>
2019-01-09 15:03:13 -0600

    Move parallax code into its own method, remove channel image

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,28 +2,28 @@ import React, { Component, Fragment } from 'react'
 import '../css/Header.css'
 
 class Header extends Component {
+  parallax = () => {
+    let starting = 1
+    let scrollPos = document.documentElement.scrollTop
+
+    if (scrollPos > 150 || window.innerWidth < 950) return
+    // Move background image up at a slower rate
+    const header = document.querySelector('.header-bg')
+    header.style.backgroundPosition = '50% ' + (50 + scrollPos / 25 + '%')
+
+    // Fade out text
+    // const descripton = document.querySelector('.description')
+    // const title = document.querySelector('.channel-title')
+
+    // title.style.opacity = starting - scrollPos / 100
+    // descripton.style.opacity = starting - scrollPos / 100
+  }
   componentDidMount() {
-    document.addEventListener('scroll', () => {
-      let starting = 1
-      let scrollPos = document.documentElement.scrollTop
+    document.addEventListener('scroll', this.parallax, true)
+  }
 
-      if (scrollPos > 150 || window.innerWidth < 950) return
-      // Move background image up at a slower rate
-      const header = document.querySelector('.header-bg')
-      header.style.backgroundPosition = '50% ' + (50 + scrollPos / 25 + '%')
-
-      // Fade out text
-      // const descripton = document.querySelector('.description')
-      // const title = document.querySelector('.channel-title')
-
-      // if (title) {
-      //   title.style.opacity = starting - scrollPos / 100
-      // }
-
-      // if (descripton) {
-      //   descripton.style.opacity = starting - scrollPos / 100
-      // }
-    })
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.parallax, true)
   }
 
   render() {
@@ -42,7 +42,6 @@ class Header extends Component {
             <div style={styles} className="header-bg" />
           </div>
         </header>
-        <img className="channel-img" src={img} alt="podcast image" />
       </Fragment>
     )
   }
