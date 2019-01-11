7fc7a96a9073d53e20969542f14937729e0dd123
Nicole <nicole.sattler@live.com>
2019-01-11 15:16:32 -0600

    Pass props for theme

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import '../css/Loader.css'
 
-const Loader = () => {
+const Loader = props => {
   return (
-    <div className="loader-wrapper">
-      <div className="loader" />
-    </div>
+    <Fragment>
+      <div className="loader-wrapper">
+        <div className="loader" />
+      </div>
+      <div className={`loader-bg ${props.theme}`} />
+    </Fragment>
   )
 }
 
