29ec65f6146713d3cab0b79223879a1a9ffbeed5
Nicole <nicole.sattler@live.com>
2019-01-13 17:11:09 -0600

    Display formatted track duration

--- src/components/PodcastListElement.js
+++ src/components/PodcastListElement.js
@@ -10,7 +10,7 @@ class PodcastListElement extends Component {
   }
 
   render() {
-    const { title, date, theme } = this.props
+    const { title, date, duration, theme } = this.props
     const isPlaying = this.props.title == this.props.nowPlaying
 
     const now = moment()
@@ -21,10 +21,17 @@ class PodcastListElement extends Component {
         ? moment(date).format('LL')
         : moment(date).fromNow()
 
+    const formattedDuration = duration.length < 8 ? '00:' + duration : duration
+    const minutesLong = Math.round(
+      moment.duration(formattedDuration).asMinutes()
+    )
+
     return (
       <div className={`infoBox ${theme}`}>
         <div className="text">
-          <p className="date">{formattedDate}</p>
+          <p className="date">
+            {formattedDate} 🞄 {minutesLong} mins
+          </p>
           <h3>{title.length > 50 ? title.substring(0, 50) + '...' : title}</h3>
         </div>
         <button
