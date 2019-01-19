3d376521db9918f57713732409b01420ed509a9c
Nicole <nicole.sattler@live.com>
2019-01-19 11:00:11 -0600

    Add episodes component

---
+++ src/components/Episodes.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import PodcastListElement from './PodcastListElement'
+
+class Episodes extends Component {
+  render() {
+    const {
+      episodes,
+      nowPlaying,
+      setAudio,
+      resetButtons,
+      theme,
+      title,
+      description,
+      img
+    } = this.props
+
+    const episodeList = this.props.episodes
+      .filter(e => e.hasOwnProperty('enclosure'))
+      .map((e, i) => (
+        <PodcastListElement
+          key={i}
+          date={e.pubDate._text}
+          title={e.title._text}
+          nowPlaying={nowPlaying}
+          audio={e.enclosure._attributes.url}
+          setAudio={setAudio}
+          resetButtons={resetButtons}
+          theme={theme}
+          duration={e['itunes:duration']._text}
+        />
+      ))
+
+    return (
+      <div className={`items ${theme}`}>
+        <div className={`channel-info ${theme}`}>
+          <img
+            className={`channel-img ${theme}`}
+            src={img}
+            alt="podcast image"
+          />
+          <h1 className="title">{title}</h1>
+          <p>{description || 'No Description Available :('}</p>
+        </div>
+        <h1 className={`episodes ${theme}`}>Episodes</h1>
+        {episodeList}
+      </div>
+    )
+  }
+}
+
+export default Episodes
