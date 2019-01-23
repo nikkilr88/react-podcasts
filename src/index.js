db2b1820308a529ea1b21959447951258c7ad3b3
Nicole <nicole.sattler@live.com>
2019-01-22 18:17:34 -0600

    Import themes CSS

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 
 import './css/styles.css'
+import './css/themes/light.css'
+import './css/themes/dark.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
