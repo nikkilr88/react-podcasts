65914e99b67c5cdb37a1f328bed4f14cde0a25cf
Nicole <nicole.sattler@live.com>
2019-05-25 15:59:01 -0500

    Add progressive image

--- src/components/SidebarImg.js
+++ src/components/SidebarImg.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import ProgressiveImage from 'react-progressive-image'
+import imgPlaceholder from '../images/image-placeholder.png'
 
-const SidebarImg = ({ handleOnClick, title, link, index, imgSrc }) => (
+const SidebarImg = ({ handleOnClick, title, dataLink, index, imgSrc }) => (
   <li
     onClick={handleOnClick}
     title={title}
-    data-link={link}
+    data-link={dataLink}
     className={index === 0 ? 'sidebar-selected' : ''}
   >
-    <img className='img-link' src={imgSrc} alt='podcast icon' />
+    <ProgressiveImage delay={1000} src={imgSrc} placeholder={imgPlaceholder}>
+      {src => <img src={src} className='img-link' alt='an image' />}
+    </ProgressiveImage>
   </li>
 )
 
