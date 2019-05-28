82cd47ff74f3d7054196b02f0d51ae1708541c85
Nicole <nicole.sattler@live.com>
2019-05-28 17:27:05 -0500

    Use current track to set active class

--- src/components/SidebarImg.js
+++ src/components/SidebarImg.js
@@ -2,12 +2,19 @@ import React from 'react'
 import ProgressiveImage from 'react-progressive-image'
 import imgPlaceholder from '../images/image-placeholder.png'
 
-const SidebarImg = ({ handleOnClick, title, dataLink, index, imgSrc }) => (
+const SidebarImg = ({
+  handleOnClick,
+  title,
+  currentTrack,
+  dataLink,
+  index,
+  imgSrc
+}) => (
   <li
     onClick={handleOnClick}
     title={title}
     data-link={dataLink}
-    className={index === 0 ? 'sidebar-selected' : ''}
+    className={title === currentTrack ? 'sidebar-selected' : ''}
   >
     <ProgressiveImage delay={1000} src={imgSrc} placeholder={imgPlaceholder}>
       {src => <img src={src} className='img-link' alt='an image' />}
