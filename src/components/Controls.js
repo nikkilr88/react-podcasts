9517a71e7529ea14e610cf1490879db7ca9b0758
Nicole <nicole.sattler@live.com>
2019-05-29 16:09:39 -0500

    Refactor, clean up code

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import ProgressBar from './ProgressBar'
-
 import Sound from 'react-sound'
 
 import '../css/Controls.css'
@@ -21,8 +20,8 @@ class Controls extends Component {
     } = this.props
 
     return (
-      <div id="player" className={theme}>
-        <div className="title">
+      <div id='player' className={theme}>
+        <div className='title'>
           {audio.title.length > 50
             ? audio.title.substring(0, 50) + '...'
             : audio.title}
@@ -30,26 +29,26 @@ class Controls extends Component {
 
         <ProgressBar position={position} duration={duration} theme={theme} />
 
-        <div className="control-btns">
-          <span className="time">{time}</span>
+        <div className='control-btns'>
+          <span className='time'>{time}</span>
           <button onClick={rewind}>
-            <i className="material-icons">replay_5</i>
+            <i className='material-icons'>replay_5</i>
           </button>
 
           <button onClick={pauseAudio}>
             {playingStatus == Sound.status.PLAYING ? (
-              <i className="material-icons paused">pause</i>
+              <i className='material-icons paused'>pause</i>
             ) : (
-              <i className="material-icons">play_arrow</i>
+              <i className='material-icons'>play_arrow</i>
             )}
           </button>
 
           <button onClick={stopAudio}>
-            <i className="material-icons">stop</i>
+            <i className='material-icons'>stop</i>
           </button>
 
           <button onClick={fastforward}>
-            <i className="material-icons">forward_10</i>
+            <i className='material-icons'>forward_10</i>
           </button>
         </div>
       </div>
