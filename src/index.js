0f063c6dfa3fbdaa15ee0332a2da08b6020e6fbe
Nicole <nicole.sattler@live.com>
2019-05-31 18:30:34 -0500

    Configure store and add provider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+import { Provider } from 'react-redux'
+import configureStore from './store/config-store'
+import { fetchPodcast } from './actions/podcast'
+
 import './css/styles.css'
 import './css/themes/light.css'
 import './css/themes/dark.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = configureStore()
+
+store.dispatch(fetchPodcast('https://feed.syntax.fm/rss'))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
