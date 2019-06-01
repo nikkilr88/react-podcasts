e8fd9b83f23d68b62797f9f18ea4934af170a23b
Nicole <nicole.sattler@live.com>
2019-06-01 17:54:03 -0500

    Change default state, add action types to switch

--- src/reducers/podcast.js
+++ src/reducers/podcast.js
@@ -1,15 +1,31 @@
 const podcastState = {
-  title: '',
-  description: '',
-  img: '',
-  episodes: []
+  podcast: {
+    title: '',
+    description: '',
+    img: '',
+    episodes: []
+  },
+  error: '',
+  loading: true
 }
 
 const podcastReducer = (state = podcastState, action) => {
   switch (action.type) {
     case 'FETCH_PODCAST':
       return {
-        ...action.podcast
+        ...state,
+        loading: false,
+        podcast: action.podcast
+      }
+    case 'SET_ERROR':
+      return {
+        ...state,
+        error: 'Uh-oh, this is taking longer than expected...'
+      }
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: true
       }
     default:
       return state
