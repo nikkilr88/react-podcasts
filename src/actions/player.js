baf493d6abfacdd1dcf93b64da64a19352052ff1
Nicole <nicole.sattler@live.com>
2019-06-03 18:14:30 -0500

    Add pause and playing actions

--- src/actions/player.js
+++ src/actions/player.js
@@ -14,6 +14,30 @@ export const setAudio = (audio, title) => dispatch => {
   })
 }
 
+export const handleOnPlaying = data => (dispatch, getState) => {
+  const state = getState()
+
+  if (state.player.playStatus === Sound.status.PLAYING) {
+    dispatch({
+      type: 'HANDLE_ON_PLAY',
+      payload: data
+    })
+  }
+}
+
+export const pauseAudio = () => (dispatch, getState) => {
+  const state = getState()
+  const paused =
+    state.player.playStatus === Sound.status.PLAYING
+      ? Sound.status.PAUSED
+      : Sound.status.PLAYING
+
+  dispatch({
+    type: 'PAUSE_AUDIO',
+    paused
+  })
+}
+
 export const stopAudio = () => dispatch => {
   dispatch({
     type: 'STOP_AUDIO'
