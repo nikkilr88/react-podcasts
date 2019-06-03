adb044407d97c2fb12e7b5beba4a5f66b920d281
Nicole <nicole.sattler@live.com>
2019-06-03 18:15:04 -0500

    Remove unused code

--- src/components/App.js
+++ src/components/App.js
@@ -11,14 +11,8 @@ import Sidebar from './Sidebar'
 import Volume from './Volume'
 import Episodes from './Episodes'
 
-import { convertSeconds } from '../utils'
-
 class App extends Component {
   state = {
-    track: { title: '', src: '' },
-    position: 0,
-    duration: 0,
-    playingStatus: Sound.status.PLAYING,
     volume: 75,
     showVolume: false,
     theme: 'light' // light or dark
@@ -29,20 +23,6 @@ class App extends Component {
     this.setState(prevState => ({
       theme: prevState.theme === 'light' ? 'dark' : 'light'
     }))
-  }
-
-  // Pause audio
-  pauseAudio = e => {
-    e && e.target.blur()
-
-    if (this.state.position > 0) {
-      this.setState(prevState => ({
-        playingStatus:
-          prevState.playingStatus == Sound.status.PLAYING
-            ? Sound.status.PAUSED
-            : Sound.status.PLAYING
-      }))
-    }
   }
 
   // Fastforward track 10 seconds
@@ -63,16 +43,6 @@ class App extends Component {
     if (this.state.position > 0) {
       this.setState(prevState => ({
         position: prevState.position - 1000 * 5
-      }))
-    }
-  }
-
-  // Handle track playback
-  handleOnPlaying = data => {
-    if (this.state.playingStatus === Sound.status.PLAYING) {
-      this.setState(() => ({
-        position: data.position,
-        duration: data.duration
       }))
     }
   }
@@ -102,7 +72,9 @@ class App extends Component {
 
   // Set volume
   setVolume = e => {
-    if (!this.state.track.src) return
+    if (!this.props.track.src) return
+
+    console.log(this.state.volume)
 
     this.showVolume()
 
@@ -154,7 +126,7 @@ class App extends Component {
 
     // Prevent spacebar scrolling
     document.addEventListener('keydown', e => {
-      if (this.state.track.src.length < 1) return
+      if (this.props.track.src.length < 1) return
 
       if (e.which == 32 || e.which == 38 || e.which == 40) {
         e.preventDefault()
@@ -184,7 +156,7 @@ class App extends Component {
             <Header />
             <Episodes
               theme={this.state.theme}
-              nowPlaying={this.state.track.title}
+              nowPlaying={this.props.track.title}
             />
           </Fragment>
         )}
@@ -194,20 +166,13 @@ class App extends Component {
             <SoundWrapper
               volume={this.state.volume}
               onError={this.handleOnError}
-              onPlaying={this.handleOnPlaying}
-              playFromPosition={this.state.position}
               onFinishedPlaying={this.handleOnFinishedPlaying}
             />
             <Controls
               rewind={this.rewind}
               theme={this.state.theme}
               volume={this.state.volume}
-              pauseAudio={this.pauseAudio}
               fastforward={this.fastforward}
-              position={this.state.position}
-              duration={this.state.duration}
-              playingStatus={this.state.playingStatus}
-              time={convertSeconds(this.state.position / 1000)}
             />
           </Fragment>
         )}
@@ -217,9 +182,9 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({
-  loading: state.podcast.loading,
+  track: state.player.track,
   error: state.podcast.error,
-  track: state.player.track
+  loading: state.podcast.loading
 })
 
 export default connect(
