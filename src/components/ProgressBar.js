5fdd264691f2a08e2514f4df95bc3fbba6b0d1e2
Nicole <nicole.sattler@live.com>
2019-06-04 18:40:29 -0500

    Connect to redux store

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import '../css/ProgressBar.css'
 
 class ProgressBar extends Component {
   render() {
     const { position, duration, theme } = this.props
+
     const styles = {
       width: (position * 100) / duration + '%'
     }
+
     return (
       <div className={`progress-wrapper ${theme}`}>
-        <div style={styles} className="progress-bar" />
+        <div style={styles} className='progress-bar' />
       </div>
     )
   }
 }
 
-export default ProgressBar
+const mapStateToProps = state => ({
+  position: state.player.position,
+  duration: state.player.duration
+})
+
+export default connect(mapStateToProps)(ProgressBar)
