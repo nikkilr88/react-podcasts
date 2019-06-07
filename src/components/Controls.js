ee0725d86d668c924beb9a59666ba5bc5cc2b75c
Nicole <nicole.sattler@live.com>
2019-06-06 19:59:24 -0500

    Add keybord shortcuts code, remove listeners on unmount

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -8,6 +8,59 @@ import { convertSeconds } from '../utils'
 import '../css/Controls.css'
 
 class Controls extends Component {
+  // Pause, skip forward / back
+  keyboardShortcuts = e => {
+    switch (e.which) {
+      case 32:
+        this.props.pauseAudio()
+        break
+      case 37:
+        this.props.skip(-10000)
+        break
+      case 39:
+        this.props.skip(10000)
+        break
+    }
+  }
+
+  // Keyup keyboard shortcuts
+  handleOnKeyUp = e => {
+    switch (e.which) {
+      case 32:
+      case 39:
+      case 37:
+        this.keyboardShortcuts(e)
+        break
+      case 38:
+      case 40:
+        // this.setVolume(e)
+        break
+    }
+  }
+
+  handleOnKeyDown = e => {
+    if (this.props.track.src.length < 1) return
+
+    if (e.which == 32 || e.which == 38 || e.which == 40) {
+      e.preventDefault()
+      return false
+    }
+  }
+
+  // Fetch podcast data on mount
+  componentDidMount() {
+    // Keyboard controls
+    document.addEventListener('keyup', this.handleOnKeyUp, false)
+
+    // Prevent spacebar scrolling
+    document.addEventListener('keydown', this.handleOnKeyDown, false)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleOnKeyUp, false)
+    document.removeEventListener('keydown', this.handleOnKeyDown, false)
+  }
+
   render() {
     const {
       time,
