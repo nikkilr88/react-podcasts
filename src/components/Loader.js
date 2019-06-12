ec968a056fc530c897bc66366e33ea0322c6f697
Nicole <nicole.sattler@live.com>
2019-06-12 12:53:23 -0500

    Connect to redux

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,20 @@
 import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
 import '../css/Loader.css'
 
 const Loader = props => {
   return (
     <Fragment>
-      <div className="loader-wrapper">
-        <div className="loader" />
+      <div className='loader-wrapper'>
+        <div className='loader' />
       </div>
       <div className={`loader-bg ${props.theme}`} />
     </Fragment>
   )
 }
 
-export default Loader
+const mapStateToProps = state => ({
+  theme: state.theme.theme
+})
+
+export default connect(mapStateToProps)(Loader)
