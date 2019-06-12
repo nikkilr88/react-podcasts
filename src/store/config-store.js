44b3bfaf7bdb04d607df3367718c377e6fe94e8b
Nicole <nicole.sattler@live.com>
2019-06-12 12:54:06 -0500

    Add theme reducer to root reducer

--- src/store/config-store.js
+++ src/store/config-store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import podcastReducer from '../reducers/podcast'
 import playerReducer from '../reducers/player'
+import themeReducer from '../reducers/theme'
 import thunk from 'redux-thunk'
 
 const initialState = {}
@@ -9,10 +10,11 @@ export default () => {
   const store = createStore(
     combineReducers({
       podcast: podcastReducer,
-      player: playerReducer
+      player: playerReducer,
+      theme: themeReducer
     }),
     compose(
-      applyMiddleware(thunk)
+      applyMiddleware(thunk),
       // window.__REDUX_DEVTOOLS_EXTENSION__ &&
       //   window.__REDUX_DEVTOOLS_EXTENSION__()
     )
