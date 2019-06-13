15bbe048a7b735708e8f5e3507e51425310fe6fc
Nicole <nicole.sattler@live.com>
2019-06-13 12:00:57 -0500

    Refactor get request, change node service

--- src/actions/podcast.js
+++ src/actions/podcast.js
@@ -11,17 +11,17 @@ export const fetchPodcast = url => dispatch => {
   axios({
     method: 'GET',
     timeout: 25 * 1000,
-    url: `https://xmlparse.glitch.me/?url=${url}`
+    url: `https://podcast-parse.glitch.me/?url=${url}`
   })
     .then(res => {
-      const channel = res.data.rss.channel
-      const title = channel.title._text || channel.title._cdata
-      const description =
-        channel.description._cdata || channel.description._text
-      const img = channel.image.url._text.replace(/http:\/\//, 'https://')
-      const episodes = channel.item.filter(e => e.hasOwnProperty('enclosure'))
-      const website = channel.link._text
-      const author = channel['itunes:author']._text
+      const channel = res.data.data
+
+      const title = channel.title
+      const website = channel.link
+      const author = channel.author
+      const episodes = channel.episodes
+      const description = channel.description.long
+      const img = channel.image.replace(/http:\/\//, 'https://')
 
       dispatch({
         type: 'FETCH_PODCAST',
