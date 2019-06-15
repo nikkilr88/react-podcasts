262d3fbc0ca0ef0b50df09b9e476b6de52a9704a
Nicole <nicole.sattler@live.com>
2019-06-15 18:16:32 -0500

    Add 404 page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment, Suspense, lazy } from 'react'
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>404</h1>
+      </div>
+    )
+  }
+}
+
+export default NotFound
