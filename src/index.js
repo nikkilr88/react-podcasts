eea291edea65df312ffb06b90e561a77a3e88cbd
Nicole <nicole.sattler@live.com>
2019-06-15 17:56:40 -0500

    Configure router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import HomePage from './pages/Home'
+import Sidebar from './components/Sidebar'
+import Controls from './components/Controls'
 
 import { Provider } from 'react-redux'
 import configureStore from './store/config-store'
-import { fetchPodcast } from './actions/podcast'
+
+import podcasts from './data/podcasts'
 
 import './css/styles.css'
 import './css/themes/light.css'
@@ -14,7 +20,14 @@ const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Sidebar list={podcasts} />
+      <Controls />
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/podcast/:podcast' exact component={App} />
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
