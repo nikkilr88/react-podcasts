72a602103794b0d9b4e60cc0c88a13460106cb4a
Nicole <nicole.sattler@live.com>
2019-06-15 16:13:09 -0500

    Remove default props

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,47 +12,6 @@ import { fetchPodcast, setLoading } from '../actions/podcast'
 import '../css/Sidebar.css'
 
 class Sidebar extends Component {
-  static defaultProps = {
-    list: [
-      {
-        name: 'Syntax - Tasty Web Development Treats',
-        link: 'https://feed.syntax.fm/rss',
-        img:
-          'https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/60/4e/d6/604ed6ba-6a5a-5faa-edf5-189a290f3aa3/source/60x60bb.jpg'
-      },
-      {
-        name: 'The freeCodeCamp Podcast',
-        link: 'http://freecodecamp.libsyn.com/rss',
-        img:
-          'https://is3-ssl.mzstatic.com/image/thumb/Music123/v4/de/83/f9/de83f9d5-1745-8572-56d9-b10f62824807/source/60x60bb.jpg'
-      },
-      {
-        name: 'Full Stack Radio',
-        link: 'https://rss.simplecast.com/podcasts/279/rss',
-        img:
-          'https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/91/8f/bc/918fbcc5-9e95-4a42-c8f4-50f2321fcc9d/source/60x60bb.jpg'
-      },
-      {
-        name: 'Tech Jr',
-        link: 'https://techjr.dev/rss.xml',
-        img:
-          'https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/bc/ae/5d/bcae5d27-c7d9-6a89-a59f-103049db812b/source/60x60bb.jpg'
-      },
-      {
-        name: 'Base.cs Podcast',
-        link: 'http://feeds.codenewbie.org/basecs_podcast.xml',
-        img:
-          'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/11/79/e5/1179e5b5-3899-2c26-175f-d4ab664c5197/source/60x60bb.jpg'
-      },
-      {
-        name: 'CodeNewbie',
-        link: 'http://feeds.codenewbie.org/cnpodcast.xml',
-        img:
-          'https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/64/02/75/640275f5-5c8e-27ba-352e-3e68449b1335/source/60x60bb.jpg'
-      }
-    ]
-  }
-
   state = {
     showSidebar: false
   }
