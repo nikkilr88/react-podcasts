b7805c6051af91c68aae3d8e02d1c4d1fced558d
Nicole <nicole.sattler@live.com>
2019-06-15 18:00:40 -0500

    Add home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment, Suspense, lazy } from 'react'
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home page</h1>
+      </div>
+    )
+  }
+}
+
+export default HomePage
