5451a51f1ef88633908e0b0b76f8a4ad4f2637fc
Nicole <nicole.sattler@live.com>
2019-06-17 16:41:08 -0500

    Render podcasts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,34 @@
-import React, { Component, Fragment, Suspense, lazy } from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { podcasts, categories } from '../data/podcasts-test'
+
+import '../css/HomePage.css'
 
 class HomePage extends Component {
   render() {
-    return (
-      <div>
-        <h1>Home page</h1>
-      </div>
-    )
+    const categorySections = categories.map(category => {
+      const categoryPodcasts = podcasts
+        .filter(podcast => podcast.category === category)
+        .map(podcast => (
+          <Link
+            className='category-podcast'
+            key={podcast.name}
+            to={`/podcast/${podcast.name.replace(/ /g, '_')}`}
+          >
+            <div>
+              <img src={podcast.img} alt={podcast.name} />
+              <h3>{podcast.name}</h3>
+            </div>
+          </Link>
+        ))
+      return (
+        <section key={category} className='podcast-category'>
+          <h2>{category}</h2>
+          <div className='category-podcasts'>{categoryPodcasts}</div>
+        </section>
+      )
+    })
+    return <div className='home-page'>{categorySections}</div>
   }
 }
 
