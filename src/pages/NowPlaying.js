2c9b9374737c97dbec45336845d51bb20c0fbf12
Nicole <nicole.sattler@live.com>
2019-06-18 16:49:28 -0500

    Add now playing page

---
+++ src/pages/NowPlaying.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Sound from 'react-sound'
+
+import { pauseAudio, skip } from '../actions/player'
+
+import '../css/NowPlaying.css'
+
+class NowPlaying extends Component {
+  render() {
+    const { img, title, podcast } = this.props.player.track
+    const { playStatus } = this.props.player
+
+    return (
+      <div>
+        {title ? (
+          <div className='mobile-player'>
+            <img src={img} alt='' />
+
+            <div>
+              <Link to={`/podcast/${podcast.replace(/ /g, '_')}`}>
+                <h4>{podcast}</h4>
+              </Link>
+              <h3>{title}</h3>
+            </div>
+
+            <div className='mobile-controls'>
+              <i
+                className='material-icons'
+                onClick={() => this.props.skip(-5000)}
+              >
+                replay_5
+              </i>
+              <i
+                className='material-icons play-btn'
+                onClick={this.props.pauseAudio}
+              >
+                {playStatus === Sound.status.PAUSED
+                  ? 'play_circle_outline'
+                  : 'pause_circle_outline'}
+              </i>
+              <i
+                className='material-icons'
+                onClick={() => this.props.skip(10000)}
+              >
+                forward_10
+              </i>
+            </div>
+          </div>
+        ) : (
+          <h1>There is nothing playing</h1>
+        )}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  player: state.player
+})
+
+export default connect(
+  mapStateToProps,
+  { pauseAudio, skip }
+)(NowPlaying)
