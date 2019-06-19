fcd7eecfca51c67d24bd3ac857b6a574302ed7e2
Nicole <nicole.sattler@live.com>
2019-06-19 18:32:07 -0500

    Add top navbar

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,27 +1,61 @@
 import React, { Component, Fragment } from 'react'
-import { Link } from 'react-router-dom'
-import HamMenu from '../images/hamburger-menu.png'
+import { connect } from 'react-redux'
+import { NavLink as Link, withRouter } from 'react-router-dom'
 import '../css/MobileNav.css'
 
 class MobileNav extends Component {
   render() {
+    const location = {
+      '/': 'Home',
+      '/nowplaying': 'Now Playing',
+      '/bookmarked': 'Bookmarked',
+      '/settings': 'Settings'
+    }
+
+    const { podcast = 'Loading...' } = this.props
+    const { pathname } = this.props.location
+
+    const formattedPodcastName =
+      podcast.length > 15 ? podcast.substring(0, 15) + '...' : podcast
+
     return (
-      <nav className='mobile-nav '>
-        <Link to='/'>
-          <i className='fas fa-home' />
-        </Link>
-        <Link to='/nowplaying'>
-          <i className='fas fa-headphones-alt' />
-        </Link>
-        <Link to='/bookmarked'>
-          <i className='fas fa-bookmark' />
-        </Link>
-        <Link to='/settings'>
-          <i className='fas fa-cog' />
-        </Link>
-      </nav>
+      <Fragment>
+        <nav className='MobileNav-top'>
+          <p className='MobileNav-title'>
+            {location[pathname] || formattedPodcastName}
+          </p>
+          {pathname !== '/' && (
+            <button
+              className='MobileNav-back'
+              onClick={() => this.props.history.goBack()}
+            >
+              <i className='fas fa-arrow-left' />
+              Back
+            </button>
+          )}
+        </nav>
+
+        <nav className='MobileNav'>
+          <Link to='/' exact activeClassName='active-nav-item'>
+            <i className='fas fa-home' />
+          </Link>
+          <Link to='/nowplaying' activeClassName='active-nav-item'>
+            <i className='fas fa-headphones-alt' />
+          </Link>
+          <Link to='/bookmarked' activeClassName='active-nav-item'>
+            <i className='fas fa-bookmark' />
+          </Link>
+          <Link to='/settings' activeClassName='active-nav-item'>
+            <i className='fas fa-cog' />
+          </Link>
+        </nav>
+      </Fragment>
     )
   }
 }
 
-export default MobileNav
+const mapStateToProps = state => ({
+  podcast: state.podcast.podcast.title
+})
+
+export default withRouter(connect(mapStateToProps)(MobileNav))
