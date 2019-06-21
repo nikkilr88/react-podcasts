8b2a6a00ba3326463d4ec1599466af598cde111c
Nicole <nicole.sattler@live.com>
2019-06-20 22:17:48 -0500

    Add function to save state to local storage

--- src/store/config-store.js
+++ src/store/config-store.js
@@ -1,10 +1,18 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import throttle from 'lodash.throttle'
 import podcastReducer from '../reducers/podcast'
 import playerReducer from '../reducers/player'
 import themeReducer from '../reducers/theme'
 import thunk from 'redux-thunk'
 
-const initialState = {}
+const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('playerState', serializedState)
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 export default () => {
   const store = createStore(
@@ -15,9 +23,18 @@ export default () => {
     }),
     compose(
       applyMiddleware(thunk),
-      // window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      //   window.__REDUX_DEVTOOLS_EXTENSION__()
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
     )
+  )
+
+  store.subscribe(
+    throttle(() => {
+      console.log('[REDUX] Saving state')
+      saveState({
+        playerState: { ...store.getState().player, playStatus: 'PAUSED' }
+      })
+    }, 1000)
   )
 
   return store
