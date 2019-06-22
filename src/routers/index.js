ab624ef1a3f863b6e1974d3f973350b384e8a9d8
Nicole <nicole.sattler@live.com>
2019-06-22 17:30:52 -0500

    Connect router component to redux store

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import HomePage from '../pages/Home'
-import NotFound from '../pages/NotFound'
+import { connect } from 'react-redux'
+
+// Components
 import Sidebar from '../components/Sidebar'
 import Controls from '../components/Controls'
-import PodcastPage from '../pages/PodcastPage'
 import MobileNav from '../components/MobileNav'
+
+// Pages
+import HomePage from '../pages/Home'
+import NotFound from '../pages/NotFound'
+import SettingsPage from '../pages/Settings'
+import PodcastPage from '../pages/PodcastPage'
 import NowPlayingPage from '../pages/NowPlaying'
 
-export default () => (
+const AppRouter = ({ theme }) => (
   <Router>
     <MobileNav />
-    <div className='app-wrapper'>
+    <div className={`app-wrapper ${theme}`}>
       <div>
         <Sidebar />
       </div>
@@ -20,6 +26,7 @@ export default () => (
         <Switch>
           <Route path='/' exact component={HomePage} />
           <Route path='/nowplaying' exact component={NowPlayingPage} />
+          <Route path='/settings' exact component={SettingsPage} />
           <Route path='/podcast/:podcast' exact component={PodcastPage} />
           <Route component={NotFound} />
         </Switch>
@@ -27,3 +34,9 @@ export default () => (
     </div>
   </Router>
 )
+
+const mapStateToProps = state => ({
+  theme: state.theme.theme
+})
+
+export default connect(mapStateToProps)(AppRouter)
