5ff306e94f8c510ebc8c310329a82f5deb20cc7a
Nicole <nicole.sattler@live.com>
2019-06-21 19:01:28 -0500

    Import router,  remove unused code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
+import AppRouter from './routers'
 import ReactDOM from 'react-dom'
-import App from './components/App'
-import HomePage from './pages/Home'
-import NotFound from './pages/NotFound'
-import Sidebar from './components/Sidebar'
-import Controls from './components/Controls'
-import MobileNav from './components/MobileNav'
-import NowPlayingPage from './pages/NowPlaying'
 
 import { Provider } from 'react-redux'
 import configureStore from './store/config-store'
 import throttle from 'lodash.throttle'
-
-import podcasts from './data/podcasts'
 
 import './css/styles.css'
 import './css/themes/light.css'
@@ -22,10 +12,12 @@ import './css/themes/dark.css'
 
 const saveState = state => {
   try {
+    console.log('[LOCAL_STORAGE] Saving state')
+
     const serializedState = JSON.stringify(state)
     localStorage.setItem('playerState', serializedState)
   } catch (err) {
-    console.log(err)
+    console.log('[LOCAL_STORAGE] Error: ', err)
   }
 }
 
@@ -33,7 +25,6 @@ const store = configureStore()
 
 store.subscribe(
   throttle(() => {
-    console.log('[REDUX] Saving state')
     saveState({
       playerState: { ...store.getState().player, playStatus: 'PAUSED' }
     })
@@ -42,23 +33,7 @@ store.subscribe(
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <MobileNav />
-      <div className='app-wrapper'>
-        <div>
-          <Sidebar list={podcasts} />
-        </div>
-        <div className='app-wrapper-content'>
-          <Controls />
-          <Switch>
-            <Route path='/' exact component={HomePage} />
-            <Route path='/nowplaying' exact component={NowPlayingPage} />
-            <Route path='/podcast/:podcast' exact component={App} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <AppRouter />
   </Provider>,
   document.getElementById('root')
 )
