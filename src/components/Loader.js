f538e0cbdfd4c90b6cff5f1d4f2ce4f829c1b97d
Nicole <nicole.sattler@live.com>
2019-06-25 17:08:24 -0500

    Remove unused code

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,14 +3,7 @@ import { connect } from 'react-redux'
 import '../css/Loader.css'
 
 const Loader = props => {
-  return (
-    <Fragment>
-      <div className='loader-wrapper'>
-        <div className='loader' />
-      </div>
-      <div className={`loader-bg ${props.theme}`} />
-    </Fragment>
-  )
+  return <div className='loader' />
 }
 
 const mapStateToProps = state => ({
